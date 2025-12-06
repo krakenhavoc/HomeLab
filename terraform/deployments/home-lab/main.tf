a0a72d149d5620cc7b9337840f194bf50a18b6bd
krakenhavoc <krakenhavoc@protonmail.com>
2025-12-05 22:21:51 -0500

    use new template

--- terraform/deployments/home-lab/main.tf
+++ terraform/deployments/home-lab/main.tf
@@ -3,9 +3,9 @@ resource "proxmox_vm_qemu" "cloudinit-example" {
   name             = "test-terraform0"
   target_node      = "pve"
   agent            = 1
-  memory           = 1024
-  boot             = "order=scsi0;ide1"   # has to be the same as the OS disk of the template
-  clone            = "ubuntu24-cloudinit" # The name of the template
+  memory           = 2048
+  boot             = "order=virtio0" # has to be the same as the OS disk of the template
+  clone            = "ubuntu-noble-template"
   scsihw           = "virtio-scsi-single"
   vm_state         = "running"
   automatic_reboot = true
@@ -14,7 +14,7 @@ resource "proxmox_vm_qemu" "cloudinit-example" {
     cores = 2
   }
   # Cloud-Init configuration
-  cicustom   = "vendor=local:snippets/qemu-guest-agent.yml" # /var/lib/vz/snippets/qemu-guest-agent.yml
+  cicustom   = "vendor=local:snippets/ubuntu.yml"
   ciupgrade  = true
   nameserver = "1.1.1.1 8.8.8.8"
   ipconfig0  = "ip=dhcp"
@@ -28,11 +28,11 @@ resource "proxmox_vm_qemu" "cloudinit-example" {
   }
 
   disks {
-    scsi {
-      scsi0 {
+    virtio {
+      virtio0 {
         # We have to specify the disk from our template, else Terraform will think it's not supposed to be there
         disk {
-          storage = "hdd_894g_thin"
+          storage = "local-lvm"
           # The size of the disk should be at least as big as the disk in the template. If it's smaller, the disk will be recreated
           size = "15G"
         }
