be480d0b7f241004ad159cbe1cf46670786508ec
krakenhavoc <krakenhavoc@protonmail.com>
2025-12-06 09:27:10 -0500

    fix k8s cloud init setup

--- terraform/deployments/home-lab/main.tf
+++ terraform/deployments/home-lab/main.tf
@@ -1,7 +1,7 @@
 module "k8s_master" {
   source = "../../modules/compute/pve-cloudinit-vm"
 
-  vm_name                         = "k8s-master-0"
+  vm_name                         = "k8s-master-1"
   memory_bytes                    = 4096
   ci_user_data                    = "vendor=local:snippets/setup_k8s_master.yml"
   cloudinit-example_root-password = var.cloudinit-example_root-password
@@ -9,7 +9,7 @@ module "k8s_master" {
 
 module "k8s_workers" {
   source   = "../../modules/compute/pve-cloudinit-vm"
-  for_each = toset(["0", "1"])
+  for_each = toset(["2", "3"])
 
   vm_name                         = "k8s-worker-${each.key}"
   memory_bytes                    = 4096
