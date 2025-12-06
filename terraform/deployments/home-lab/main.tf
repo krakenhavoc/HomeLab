197b8f01890d323995e2e036bc4ca5f30b777af9
krakenhavoc <krakenhavoc@protonmail.com>
2025-12-06 07:26:46 -0500

    Add K8s worker node setup

--- terraform/deployments/home-lab/main.tf
+++ terraform/deployments/home-lab/main.tf
@@ -11,8 +11,8 @@ module "k8s_workers" {
   source   = "../../modules/compute/pve-cloudinit-vm"
   for_each = toset(["0", "1"])
 
-  vm_name      = "k8s-worker-${each.key}"
-  memory_bytes = 4096
-  #ci_user_data =
+  vm_name                         = "k8s-worker-${each.key}"
+  memory_bytes                    = 4096
+  ci_user_data                    = "vendor=local:snippets/setup_k8s_worker.yml"
   cloudinit-example_root-password = var.cloudinit-example_root-password
 }
