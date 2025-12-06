b25d4007f1ea9339ec211f248c367a1cf27559e9
krakenhavoc <krakenhavoc@protonmail.com>
2025-12-05 23:49:34 -0500

    add k8s master setup script

--- terraform/deployments/home-lab/main.tf
+++ terraform/deployments/home-lab/main.tf
@@ -1,66 +1,10 @@
-resource "proxmox_vm_qemu" "cloudinit-example" {
-  vmid             = 102
-  name             = "test-terraform0"
-  target_node      = "pve"
-  agent            = 1
-  memory           = 2048
-  boot             = "order=virtio0" # has to be the same as the OS disk of the template
-  clone            = "ubuntu-noble-template"
-  scsihw           = "virtio-scsi-single"
-  vm_state         = "running"
-  automatic_reboot = true
-
-  cpu {
-    cores = 2
-  }
-  # Cloud-Init configuration
-  cicustom   = "vendor=local:snippets/ubuntu.yml"
-  ciupgrade  = true
-  nameserver = "1.1.1.1 8.8.8.8"
-  ipconfig0  = "ip=dhcp"
-  skip_ipv6  = true
-  ciuser     = "root"
-  cipassword = var.cloudinit-example_root-password
-
-  # Most cloud-init images require a serial device for their display
-  serial {
-    id = 0
-  }
-
-  disks {
-    virtio {
-      virtio0 {
-        # We have to specify the disk from our template, else Terraform will think it's not supposed to be there
-        disk {
-          storage = "local-lvm"
-          # The size of the disk should be at least as big as the disk in the template. If it's smaller, the disk will be recreated
-          size = "15G"
-        }
-      }
-    }
-    ide {
-      # Some images require a cloud-init disk on the IDE controller, others on the SCSI or SATA controller
-      ide1 {
-        cloudinit {
-          storage = "local-lvm"
-        }
-      }
-    }
-  }
-
-  network {
-    id     = 0
-    bridge = "vmbr0"
-    model  = "virtio"
-  }
-}
-
 module "k8s_master" {
   source = "../../modules/compute/pve-cloudinit-vm"
 
-  vm_name      = "k8s-master-0"
-  memory_bytes = 4096
-  #ci_user_data =
+  vm_name                         = "k8s-master-0"
+  memory_bytes                    = 4096
+  ci_user_data                    = "vendor=local:snippets/setup_k8s_master.yml"
+  cloudinit-example_root-password = var.cloudinit-example_root-password
 }
 
 module "k8s_workers" {
@@ -70,4 +14,5 @@ module "k8s_workers" {
   vm_name      = "k8s-worker-${each.key}"
   memory_bytes = 4096
   #ci_user_data =
+  cloudinit-example_root-password = var.cloudinit-example_root-password
 }
