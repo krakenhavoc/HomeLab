1a516dbd20ddaf186126f0dd2d4c23bf34394c12
krakenhavoc <krakenhavoc@protonmail.com>
2025-12-20 18:11:56 -0500

    get value of name prefix

--- terraform/deployments/home-lab/main.tf
+++ terraform/deployments/home-lab/main.tf
@@ -2,7 +2,7 @@ module "k8s_controlplane" {
   source   = "../../modules/compute/pve-cloudinit-vm"
   for_each = toset([for i in range(var.k8s_controlplane.node_count) : tostring(i)])
 
-  vm_name                         = "var.k8s_controlplane.name_prefix-${each.key}"
+  vm_name                         = "${var.k8s_controlplane.name_prefix}-${each.key}"
   memory_bytes                    = var.k8s_controlplane.memory_bytes
   ci_user_data                    = "vendor=local:snippets/setup_k8s_master.yml"
   cloudinit-example_root-password = var.cloudinit-example_root-password
@@ -12,7 +12,7 @@ module "k8s_workers" {
   source   = "../../modules/compute/pve-cloudinit-vm"
   for_each = toset([for i in range(var.k8s_worker.node_count) : tostring(i)])
 
-  vm_name                         = "var.k8s_worker.name_prefix-${each.key}"
+  vm_name                         = "${var.k8s_worker.name_prefix}-${each.key}"
   memory_bytes                    = var.k8s_worker.memory_bytes
   ci_user_data                    = "vendor=local:snippets/setup_k8s_worker.yml"
   cloudinit-example_root-password = var.cloudinit-example_root-password
