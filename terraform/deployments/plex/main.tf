4d7536c6a925d89db93bb7fff1efba1e264ece3e
krakenhavoc <krakenhavoc@protonmail.com>
2026-01-14 18:48:42 -0500

    restructure and add plex docker compose

--- terraform/deployments/plex/main.tf
+++ terraform/deployments/plex/main.tf
@@ -5,9 +5,10 @@ resource "proxmox_virtual_environment_file" "plex_cloudinit" {
   node_name    = var.pve.host
 
   source_raw {
-    data = templatefile("${path.module}/setup-plex.yaml.tftpl", {
+    data = templatefile("${path.module}/templates/setup-plex.yaml.tftpl", {
       hostname       = var.plex_host.name_prefix,
       admin_username = "plex"
+      docker_compose = local.docker_compose
     })
     file_name = "setup-plex-${var.plex_host.env}.yaml"
   }
