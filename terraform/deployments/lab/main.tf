d0864fc7b4b1d3d692c06b3dddd1b034bdb75ec9
krakenhavoc <krakenhavoc@protonmail.com>
2026-03-18 09:07:35 -0400

    fix: nested virt

--- terraform/deployments/lab/main.tf
+++ terraform/deployments/lab/main.tf
@@ -98,7 +98,7 @@ resource "proxmox_virtual_environment_vm" "windows11" {
     type    = "host"
     cores   = var.windows11.cpu_cores
     sockets = 1
-    flags   = ["+vmx"]
+    flags   = ["+nested-virt"]
   }
 
   memory {
